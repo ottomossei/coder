dfffa54bc7c7541f9edaba5aaddecab00d03fcb6
ottomossei <seki.jobhunting@gmail.com>
2021-12-12 22:02:46 +0900

    Add debug code

---
+++ workplace/atcoder/debug/debug.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+    int x;
+    cin >> x;
+    cout << x << endl;
+    cout << "hello by debug.cpp" << endl;
+}
