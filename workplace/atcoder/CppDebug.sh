06a15bf7aab62cc2828f4fc03d75951c6682d401
ottomossei <seki.jobhunting@gmail.com>
2021-12-12 21:51:03 +0900

    Bug fix for docker

--- workplace/atcoder/CppDebug.sh
+++ workplace/atcoder/CppDebug.sh
@@ -3,8 +3,8 @@ DEBUG=$CURRENT"/debug/";
 cd $DEBUG;
 ESC=$(printf '\033');
 printf "${ESC}[31m%s${ESC}[m\n" '---C++ Compile---';
-make;
+docker exec -it gcc g++ workplace/atcoder/debug/debug.cpp -o debug
 printf "${ESC}[33m%s${ESC}[m\n" '---Input---';
 echo $(<sample_input.txt);
 printf "${ESC}[33m%s${ESC}[m\n" '---Output---';
-time ./debug < sample_input.txt;
+docker exec -i gcc time -p ./debug < sample_input.txt;
